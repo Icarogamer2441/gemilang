06f94bd867b5cb7a7004b9abaf278752b2612c0d
Jose icaro <icarojose533@gmail.com>
2024-08-13 11:27:52 -0300

    Updating gemilang.py file!

--- gemilang.py
+++ gemilang.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 import google.generativeai as genai
+import time
+import os
+import platform
 
+def clear_screen():
+    system = platform.system()
+
+    if system == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
 API = "YOUR GEMINI API"
 genai.configure(api_key=API)
@@ -10,17 +20,14 @@ model = genai.GenerativeModel("gemini-pro")
 chat = model.start_chat(history=[])
 
 # is in portuguese but the gemini can translate to english if you put english things in your code
-chat.send_message("todo conteudo de arquivo que eu mandar você interpreta como de uma linguagem de programação e executa mostrando o output")
+chat.send_message("todo conteudo de arquivo que eu mandar você interpreta como de uma linguagem de programação e executa mostrando o output desse jeito:\noutput:\n```\n<output>\n```\n\nexit code: <codigo de saida>\nnunca mostre o codigo input!")
 
-
-print("use 'exit' or 'sair' to exit")
+print("press Ctrl+C to stop the program")
+fname = input("File > ")
 while True:
-    fname = input("File name > ")
-
-    if fname == "exit" or fname == "sair":
-        break
-
+    input("Press any key to execute...")
+    clear_screen()
     with open(fname, "r") as f:
         code = f.read()
     response = chat.send_message(code)
-    print("Out:", response.text, "\n")
+    print(response.text, "\n")
