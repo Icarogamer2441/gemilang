17e95b56b3e000ae59a30cfdc11f3b302b94f5bd
Jose icaro <icarojose533@gmail.com>
2024-08-13 21:32:05 -0300

    Updating the gemilang.py file!

--- gemilang.py
+++ gemilang.py
@@ -19,15 +19,20 @@ model = genai.GenerativeModel("gemini-pro")
 
 chat = model.start_chat(history=[])
 
-# is in portuguese but the gemini can translate to english if you put english things in your code
-chat.send_message("todo conteudo de arquivo que eu mandar você interpreta como de uma linguagem de programação e executa mostrando o output desse jeito:\noutput:\n```\n<output>\n```\n\nexit code: <codigo de saida>\nnunca mostre o codigo input!")
-
 print("press Ctrl+C to stop the program")
 fname = input("File > ")
+inter = int(input("Is interpreted (1 - interpreted, 0 or other - compiled)? > "))
+langname = input("Your lang name > ")
+# is in portuguese but the gemini can translate to english if you put english things in your code
+if inter == 1:
+    chat.send_message("todo conteudo de arquivo que eu mandar você interpreta como de uma linguagem de programação, ela se chama {} e executa mostrando o output desse jeito:\noutput:\n```\n<output>\n```\n\nexit code: <codigo de saida>".format(langname))
+else:
+    compname = input("Compiles to the language > ")
+    chat.send_message("todo conteudo de arquivo que eu mandar você interpreta como de uma linguagem de programação, ela se chama {} e executa mostrando o output desse jeito:\noutput:\n```\n<output>\n```\n\nexit code: <codigo de saida>\na linguagem compila para '{}' e o output mostra o codigo compilado.".format(langname, compname))
 while True:
     input("Press any key to execute...")
-    clear_screen()
     with open(fname, "r") as f:
         code = f.read()
+    clear_screen()
     response = chat.send_message(code)
     print(response.text, "\n")
